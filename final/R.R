ca98ac0bcde428a712e52b9fc50b714be5e35a00
jessicaannlee <jessicaannlee@gmail.com>
2014-05-12 11:48:45 -0400

    Work in progress
    For final deliverable

---
+++ final/R.R
@@ -0,0 +1,13 @@
+############################################################################################
+# Clear workspace 
+rm(list = ls())
+
+# working directory
+
+setwd("/Users/Jessica/Documents/Data Vis/Final Project/voxgov/final/")
+
+# Load libraries
+library("igraph")
+require("igraph")
+library("dplyr")
+require("dplyr")
